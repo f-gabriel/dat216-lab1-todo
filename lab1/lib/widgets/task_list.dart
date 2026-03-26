6d7998f2dece0085e2de226b3e0476dd270f9246
Fredrik <fredn@chalmers.se>
2026-03-26 17:05:26 +0100

    har skapat checkbox

--- lab1/lib/widgets/task_list.dart
+++ lab1/lib/widgets/task_list.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/model/task_handler.dart';
+import 'package:lab1/widgets/status_icon.dart';
+import 'package:provider/provider.dart';
 
 class TaskList extends StatelessWidget{
   const TaskList({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    var taskHandler = context.watch<TaskHandler>();
+    var tasks = taskHandler.testTasks();
+    tasks[1].completed = true;
+
     return ListView(
       children: [
-        ListTile(title: Text('DAT216')),
-        ListTile(title: Text('Lab 1')),
-        ListTile(title: Text('En till rad'))
-      ],
+        for(final task in tasks) 
+          ListTile(
+            leading: StatusIcon(task),
+            title: Text(task.title)),
+      ]
     ); 
   }
-  
 }
